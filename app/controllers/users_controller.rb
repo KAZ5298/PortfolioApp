7b13280c76767179d19e46fef444cbdb0c9b9610
EC2 Default User <ec2-user@ip-172-31-46-130.ap-northeast-1.compute.internal>
2021-04-19 20:21:59 +0000

    お気に入り機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
+  def favorites
+    @user = User.find(params[:id])
+  end
 
   # GET /users/new
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
   resource :registrations, only: [:new, :create]
   resource :sessions, only: [:new, :create, :destroy]
   resource :settings, only: [:edit, :update]
-  resources :users, only: [:index, :show]
-  resources :tweets
+  
+  resources :users, only: [:index, :show] do
+    get :favorites, on: :member
+  end
+  
+  resources :tweets do
+    resources :favorites, only: [:create, :destroy]
+  end
+  
   root to: 'registrations#new'
 end
