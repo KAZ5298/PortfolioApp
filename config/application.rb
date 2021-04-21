fda751f08e0e835d47c72e250904bc930955888f
KAZ5298 <ec2-user@ip-172-31-46-130.ap-northeast-1.compute.internal>
2021-04-21 11:19:33 +0000

    heroku

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,9 @@ module Portfolioapp
     
     config.i18n.default_locale = :ja
     
-    config.assets.initialize_on_precompile = false
+    
     
   end
 end
+
+config.assets.initialize_on_precompile = false
